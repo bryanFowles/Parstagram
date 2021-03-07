e9406292d439fc0253568d806c5ebcd8c853956a
bryan f <bfowles@csumb.edu>
2021-03-07 14:02:00 -0800

    added assets to home screen, made simple outlets

---
+++ Parstagram/LoginViewController.swift
@@ -0,0 +1,41 @@
+//
+//  LoginViewController.swift
+//  Parstagram
+//
+//  Created by user190937 on 3/7/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func inSignIn(_ sender: Any) {
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
